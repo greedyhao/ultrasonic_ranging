5ac873ab07b39f1d01430ef6f7061797874d2d40
greedyhao <hao_kr@163.com>
2019-11-07 23:42:04 +0800

    Alarm system come closer 报警系统现在距离缩短会变得急促 但是反应慢，出错率高

--- src/main.c
+++ src/main.c
@@ -13,10 +13,9 @@
 #include <math.h>
 
 /* macro */
-#define sound_velocity 34300  	/* sound velocity in cm per second */
-
+#define sound_velocity 34300  			/* sound velocity in cm per second */
 #define period_in_us pow(10,-6)
-#define Clock_period 1.085*period_in_us		/* period for clock cycle of 8051*/
+#define Clock_period 1.085*period_in_us	/* period for clock cycle of 8051*/
 
 #define SEG_PORT	P1
 
@@ -31,10 +30,11 @@ sbit ECHO_PIN	= P2^6;
 
 /* user define */
 code unsigned char seg_table[]={0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90,0x88,0x83,0xc6,0xa1,0x86,0x8e};
-unsigned int num = 0;			/* the number display on 7-segment */
+unsigned int num = 0;			/* the number displayed on 7-segment */
 unsigned char time_out = 0;		/* ECHO port */
-unsigned char beep_frq = 0;
-unsigned char beep_flag = 0;	/* status of beep 0: no work 1: short beep 2: long beep */
+unsigned char beep_frq = 0;		/* counting the beep pin toggle time */
+unsigned char beep_lv = 0;		/* level of rapid (0-4) */
+unsigned char beep_flag = 0;	/* status of beep 0: no work 1: rapid beep 2: long beep */
 
 /* static func */
 
@@ -70,20 +70,19 @@ void main(void){
 		value = Clock_period * sound_velocity; 
 		distance_measurement = (TL0|(TH0<<8));	/* read timer register for time count */
 		distance_measurement = (distance_measurement*value)/2.0;  /* find distance(in cm) */
-		
+
 		num = (int)(distance_measurement*10)%10000;
 
-		// if	 (num < 800) beep_frq = (num - 300)/25; /* (x - 300)/500*20  (level 20) */
-		// else BEEP_PIN = 0;
-		// if (num <= 300) beep_frq = 30;
-
-		if (num < 800 && num > 300) beep_flag = 1;
+		if (num <= 800 && num > 300) {
+			beep_flag = 1;
+			beep_lv = (num - 300)/100; /* (x - 300)/500*5  (level 5) */
+		}
 		else if (num <= 300) beep_flag = 2;
 		else beep_flag = 0;
 
 		ET1 = 1;
 
-		delay_ms(50);
+		delay_ms(100);
 	}
 }
 
@@ -117,7 +116,7 @@ void init_timer(void)
 	TF0		= 0;
 	TR0 	= 0;
 
-	TH1 = 0xB8;
+	TH1 = 0xB8; /* 20ms */
     TL1 = 0x00;
     EA = 1;
     ET1 = 1;
@@ -146,6 +145,12 @@ void uw_sen_start(void)
 }
 
 /* ------------ segment --------------- */
+/**
+ * @brief display number on 7-segment
+ * 
+ * @param num number format: xxx.x
+ * @note num must less than 9999
+ */
 void seg_dis(unsigned int num)
 {
 	unsigned char _bit0, _bit1, _bit2, _bit3;
@@ -184,17 +189,12 @@ void seg_dis(unsigned int num)
 
 void Timer1Interrupt(void) interrupt 3
 {
-    TH1 = 0xB8;
+    TH1 = 0xB8; /* 20ms */
     TL1 = 0x00;
-    //add your code here!
-	// --beep_frq;
+
 	seg_dis(num);
-	// if (beep_frq == 0) BEEP_PIN = !BEEP_PIN;
-	// if (beep_frq >= 20)
-	// {
-	// 	beep_frq++;
-	// 	BEEP_PIN = 1;
-	// }
+	++beep_frq;
+	++time_out;
 
 	switch (beep_flag)
 	{
@@ -202,7 +202,27 @@ void Timer1Interrupt(void) interrupt 3
 		BEEP_PIN = 0;
 		break;
 	case 1:
-		BEEP_PIN = !BEEP_PIN;
+		switch (beep_lv)
+		{
+		case 0:
+			if (beep_frq == 1) {BEEP_PIN = !BEEP_PIN; beep_frq = 0;}
+			break;
+		case 1:
+			if (beep_frq == 5) {BEEP_PIN = !BEEP_PIN; beep_frq = 0;}
+			break;
+		case 2:
+			if (beep_frq == 10) {BEEP_PIN = !BEEP_PIN; beep_frq = 0;}
+			break;
+		case 3:
+			if (beep_frq == 15) {BEEP_PIN = !BEEP_PIN; beep_frq = 0;}
+			break;
+		case 4:
+			if (beep_frq == 20) {BEEP_PIN = !BEEP_PIN; beep_frq = 0;}
+			break;
+		default:
+			if (beep_frq == 20) {BEEP_PIN = !BEEP_PIN; beep_frq = 0;}
+			break;
+		}
 		break;
 	case 2:
 		BEEP_PIN = 1;
@@ -210,6 +230,4 @@ void Timer1Interrupt(void) interrupt 3
 	default:
 		break;
 	}
-
-	time_out++;
 }
